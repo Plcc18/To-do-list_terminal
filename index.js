5a99b3679ef8e299bc66c875c6dc5fa743b2776d
Plcc18 <pedrolucascabral4@gmail.com>
2024-09-11 22:07:48 -0300

    apresenta metas realizadas

--- index.js
+++ index.js
@@ -45,7 +45,23 @@ const listarMetas = async () => {
     meta.checked = true
   })
 
-  console.log("Meta(a) concluída(s)")
+  console.log("Meta(s) marcada(s) como concluída(s)")
+}
+
+const metasRealizadas = async () => {
+  const realizadas = metas.filter((meta) => {
+    return meta.checked
+  })
+
+  if(realizadas.length == 0) {
+    console.log("Não existem metas realizadas :(")
+    return
+  }
+
+  await select({
+    message: "Metas realizadas",
+    choices: [...realizadas]
+  })
 }
 
 const start = async () => {
@@ -64,6 +80,10 @@ const start = async () => {
           value: "listar"
         },
         {
+          name: "Metas realizadas",
+          value: "realizadas"
+        },
+        {
           name: "Sair",
           value: "sair"
         }
@@ -78,6 +98,9 @@ const start = async () => {
         break
       case "listar":
         await listarMetas()
+        break
+      case "realizadas":
+        await metasRealizadas()
         break
       case "sair":
         console.log("Até a próxima")
